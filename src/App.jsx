3a34c715dd02b9650e1d62195e413121c8965ef8
Zabeehullah Yadgar <yadgar1982@gmail.com>
2026-08-03 22:32:43 -0400

    fixe backup import case

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import ProtectedLayout from "./components/Shared/Layouts/ProtectedLayout"
 import ProtectedAdminLayout from "./components/Shared/Layouts/ProtectedAdminLayout"
 import NotFound from "./components/Shared/NotFound";
 import Commissions from "./components/User/Comission";
-import Backup from "./components/Shared/Backup/Backup.jsx";
+import Backup from "./components/Shared/Backup/backup";
 
 const App = () => {
   return (
